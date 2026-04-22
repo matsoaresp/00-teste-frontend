6c07e4ab2d02d2869540ebeb1803fd87e18b7589
Mateus Prado <maateussp@gmail.com>
2026-04-22 16:01:00 -0300

    feat: configurando o codigo

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './index.css'
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-purple-500">
+      Tailwind funcionando 🚀
+    </h1>
+    </>
+  )
+}
+
+export default App
